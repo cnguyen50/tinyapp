9ec532c19fa6ea66fd6d6ff3e98d80a5ba9a3292
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-01-22 02:51:25 +0000

    with random function

--- express_server.js
+++ express_server.js
@@ -1,8 +1,22 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const PORT = 8080;
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
+
+
+const generateRandomString = function() {
+    let result = "";
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let charactersLength = chars.length;
+    for (let i = 1; i < 7; i++) {
+      result += chars.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -15,6 +29,10 @@ app.get("/hello", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -30,7 +48,13 @@ app.get("/urls", (req, res) => {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-  
+
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); 
+  res.send("Ok");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
