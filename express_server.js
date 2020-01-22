2c00ea5e1d1c1fec0fec45066a0ad26864b00b4d
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-01-22 02:03:45 +0000

    with templateVars

--- express_server.js
+++ express_server.js
@@ -17,6 +17,11 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
