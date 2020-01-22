930c433e4e35e08c0b33ce6a41bccf9a016eb645
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-01-22 02:18:40 +0000

    short url with show template

--- express_server.js
+++ express_server.js
@@ -17,6 +17,11 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
