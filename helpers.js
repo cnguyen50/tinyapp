2bfbdf8ac90f674dba309436e1102e411bfee402
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-01-24 09:44:15 +0000

    exported checkEmail func

---
+++ helpers.js
@@ -0,0 +1,11 @@
+const checkEmail = function(email) {
+    for (let user in users) {
+      if (users[user].email === email) {
+        return users[user];
+      }
+    }
+    return undefined;
+  };
+  
+
+  module.exports = { checkEmail };
