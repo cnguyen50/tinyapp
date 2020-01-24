aa238982819c2d6379b609280936504d4d1cafc7
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-01-24 10:03:57 +0000

    exported funcs

--- helpers.js
+++ helpers.js
@@ -1,11 +1,32 @@
-const checkEmail = function(email) {
-    for (let user in users) {
-      if (users[user].email === email) {
-        return users[user];
-      }
+const generateRandomString = function() {
+  let result = "";
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let charactersLength = chars.length;
+    for (let i = 1; i < 7; i++) {
+        result += chars.charAt(Math.floor(Math.random() * charactersLength));
     }
-    return undefined;
-  };
-  
+  return result;
+};
 
-  module.exports = { checkEmail };
+
+const checkEmail = function(email) {
+  for (let user in users) {
+    if (users[user].email === email) {
+      return users[user];
+    }
+  }
+  return undefined;
+};
+
+const urlsForUser = function(id) {
+let match = {};
+for (let key in urlDatabase) {
+    if (id === urlDatabase[key].userID) {
+    match[key] = urlDatabase[key].longURL;
+    }
+  }
+  return match;
+};  
+
+module.exports = { 
+checkEmail, urlsForUser, generateRandomString };
